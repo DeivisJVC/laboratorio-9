61d210a80ff8703c83730239f64c92d463817c2c
Deivis Alfonso Yance Rosado <137195450+DeivisJVC@users.noreply.github.com>
2024-11-29 17:16:51 -0500

    sql

---
+++ sql.sql
@@ -0,0 +1,19 @@
+-- Actividad 2
+--paso 1
+CREATE TABLE `Estudiante`(
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    `nombres` VARCHAR(50) NOT NULL,
+    `apellidos` VARCHAR(50) NOT NULL,
+    `edad` INT,
+    `ciudad` VARCHAR(50)
+);
+INSERT INTO Estudiante VALUE(1,"Deivis","Yance",18,"Santa Marta");
+INSERT INTO Estudiante VALUE(2,"Luis","Rodriguez",19,"Barranquilla");
+INSERT INTO Estudiante VALUE(3,"Maria","Cabana",20,"Cartagena");
+INSERT INTO Estudiante VALUE(4,"Yudis","Rosado",35,"Medellin");
+INSERT INTO Estudiante VALUE(5,"Jose","Yance",40,"Amazonas");
+INSERT INTO Estudiante VALUE(6,"Emiliano","Suarez",50,"Huila");
+INSERT INTO Estudiante VALUE(7,"Laura","Martinez",25,"Santa Marta");
+
+SELECT * FROM Estudiante
+SELECT * FROM Estudiante WHERE 
